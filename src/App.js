f93dc13bca96902d6ea029d475cc6113f6271d55
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2019-02-11 16:32:44 -0600

    create app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './App.css';
-
-class Card extends React.Component {
-  render() {
-    return <div className="card"> </div>;
-  }
-}
+import Card from './Card';
 
 class App extends React.Component {
-  render() {
-    return <h1> Hello, World! </h1>;
-  }
+	render() {
+		return (
+			<div>
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		);
+	}
 }
 
-export default Card
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Card extends React.Component {
+  render() {
+    const cardStyle = {
+      display: 'flex',
+      width: '14rem',
+      height: '18rem',
+      boxShadow: '1px 1px #7589a8',
+    }
+    return (
+      <div style={cardStyle}> 
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+export default Card
